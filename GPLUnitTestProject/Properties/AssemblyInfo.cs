46c1049a48a40d39c7884b4dcaa5e8ff9957b8a5
Pradip Dkl <pradipdkl46@gmail.com>
2020-04-03 12:40:44 +0545

    Testing Done for GPL

---
+++ GPLUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GPLUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GPLUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2e04cdd8-c505-41be-bb59-bfbe265e259d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GPLUnitTestProject/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows.Forms;
+using GPL_Appn;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GPLUnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ChkTriangleValidity()
+        {
+            Triangle T = new Triangle();
+            T.GetValue(1, 2, 0, 3);
+            bool exceptedResult = false;
+            bool actualResult = T.chkTriangleValidity();
+            Assert.AreEqual(exceptedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ChkTriangleValidity1()
+        {
+            Triangle T = new Triangle();
+            T.GetValue(50, 50, 50, 50);
+            bool exceptedResult = true;
+            bool actualResult = T.chkTriangleValidity();
+            Assert.AreEqual(exceptedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Validation()
+        {
+            TextBox TB = new TextBox();
+            TB.Text = "Circle 50";
+            Validation V = new Validation(TB);
+            bool exceptedResult = true;
+            bool actualResult = V.IsCmdValid;
+            Assert.AreEqual(exceptedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Validation1()
+        {
+            TextBox TB = new TextBox();
+            TB.Text = "Circle 50,50";
+            Validation V = new Validation(TB);
+            bool exceptedResult = false;
+            bool actualResult = V.IsCmdValid;
+            Assert.AreEqual(exceptedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void CheckCmdLineValidation()
+        {
+            TextBox TB = new TextBox();
+            TB.Text = "moveto 60,40";
+            Validation V = new Validation(TB);
+            bool expectedResult = true;
+            bool actualResult;
+            V.CheckCmdLineValidation(TB.Text);
+            actualResult = V.IsSyntaxValid;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
